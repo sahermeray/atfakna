1d3318cde23d5dabb46794d480c4e59864c32e76
ahmad <a.ahmad@outlook.de>
2020-12-24 20:23:58 +0100

    cleaning up

--- app/src/main/java/com/saher/authapp/activity/LoginActivity.java
+++ app/src/main/java/com/saher/authapp/activity/LoginActivity.java
@@ -1,9 +1,5 @@
 package com.saher.authapp.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,7 +20,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.saher.authapp.R;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText loginEmailId,logInpasswd;
+    EditText loginEmailId, logInpasswd;
     Button btnLogIn;
     TextView signup;
     FirebaseAuth firebaseAuth;
@@ -32,61 +32,61 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        logintoolbar=findViewById(R.id.login_activity_toolbar);
+        logintoolbar = findViewById(R.id.login_activity_toolbar);
         setSupportActionBar(logintoolbar);
-        firebaseAuth=FirebaseAuth.getInstance();
-        loginEmailId=findViewById(R.id.loginEmail);
-        logInpasswd=findViewById(R.id.loginpaswd);
-        btnLogIn=findViewById(R.id.btnLogIn);
-        signup=findViewById(R.id.TVSignIn);
-        authStateListener=new FirebaseAuth.AuthStateListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginEmailId = findViewById(R.id.loginEmail);
+        logInpasswd = findViewById(R.id.loginpaswd);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        signup = findViewById(R.id.TVSignIn);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user=firebaseAuth.getCurrentUser();
-                if(user!=null){
-                    Toast.makeText(LoginActivity.this,"user logged in",Toast.LENGTH_LONG).show();
-                    Intent i=new Intent(LoginActivity.this,HomeActivity.class);
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, "user logged in", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
                     startActivity(i);
-                }else{
-                    Toast.makeText(LoginActivity.this,"login to continu",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "login to continue", Toast.LENGTH_LONG).show();
                 }
             }
         };
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(LoginActivity.this,HomeActivity.class);
+                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(i);
             }
         });
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userEmail=loginEmailId.getText().toString();
-                String userPaswd=logInpasswd.getText().toString();
-                if(userEmail.isEmpty()){
+                String userEmail = loginEmailId.getText().toString();
+                String userPaswd = logInpasswd.getText().toString();
+                if (userEmail.isEmpty()) {
                     loginEmailId.setError("provide your email first");
                     loginEmailId.requestFocus();
-                }else if (userPaswd.isEmpty()){
-                        logInpasswd.setError("enter password");
-                        logInpasswd.requestFocus();
-                    }else if(userEmail.isEmpty()&&userPaswd.isEmpty()){
-                    Toast.makeText(LoginActivity.this,"fields empty",Toast.LENGTH_LONG).show();
-                }else if(!(userEmail.isEmpty()&&userPaswd.isEmpty())){
-                    firebaseAuth.signInWithEmailAndPassword(userEmail,userPaswd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                } else if (userPaswd.isEmpty()) {
+                    logInpasswd.setError("enter password");
+                    logInpasswd.requestFocus();
+                } else if (userEmail.isEmpty() && userPaswd.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "fields empty", Toast.LENGTH_LONG).show();
+                } else if (!(userEmail.isEmpty() && userPaswd.isEmpty())) {
+                    firebaseAuth.signInWithEmailAndPassword(userEmail, userPaswd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Toast.makeText(LoginActivity.this,"not successful",Toast.LENGTH_LONG).show();
-                            }else{
-                                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "not successful", Toast.LENGTH_LONG).show();
+                            } else {
+                                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                             }
                         }
                     });
-                }else{
-                    Toast.makeText(LoginActivity.this,"error",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "error", Toast.LENGTH_LONG).show();
                 }
-                }
+            }
 
         });
 
