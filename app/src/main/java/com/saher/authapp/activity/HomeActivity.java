0da9bc13ccd208ca3a761596629b90b541f65558
sahermeray <alwayswithgunners@gmail.com>
2020-12-30 21:40:58 +0200

    create account and login

--- app/src/main/java/com/saher/authapp/activity/HomeActivity.java
+++ app/src/main/java/com/saher/authapp/activity/HomeActivity.java
@@ -22,12 +22,16 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.saher.authapp.R;
 
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private SearchView.OnQueryTextListener queryTextListener;
+    FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        firebaseAuth=FirebaseAuth.getInstance();
         final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -43,6 +48,28 @@ public class HomeActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
+
+        FirebaseUser useriuser=firebaseAuth.getCurrentUser();
+        if(useriuser!=null){
+            navigationView.getMenu().getItem(1).setVisible(true);
+            navigationView.getMenu().getItem(0).setVisible(true);
+            navigationView.getMenu().getItem(2).setVisible(true);
+            navigationView.getMenu().getItem(3).setVisible(true);
+            navigationView.getMenu().getItem(4).setVisible(false);
+        }else{
+            navigationView.getMenu().getItem(1).setVisible(false);
+            navigationView.getMenu().getItem(0).setVisible(true);
+            navigationView.getMenu().getItem(2).setVisible(false);
+            navigationView.getMenu().getItem(3).setVisible(false);
+            navigationView.getMenu().getItem(4).setVisible(true);
+        }
+
+
+
+
+
+
+
         final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -57,6 +84,9 @@ public class HomeActivity extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
+                }else if(id==R.id.nav_create_account){
+                    Intent i=new Intent(getApplicationContext(),SignUpActivity.class);
+                    startActivity(i);
                 }
                 //This is for maintaining the behavior of the Navigation view
                 NavigationUI.onNavDestinationSelected(menuItem, navController);
