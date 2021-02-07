c5fa4c6126308fed9bd53dc9acc8de17f515c032
ahmad <a.ahmad@outlook.de>
2021-02-08 00:08:51 +0100

    test fb login

--- app/src/main/java/com/saher/authapp/activity/LoginActivity.java
+++ app/src/main/java/com/saher/authapp/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,15 +12,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.saher.authapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class LoginActivity extends AppCompatActivity {
     EditText loginEmailId, logInpasswd;
@@ -28,11 +46,18 @@ public class LoginActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseAuth.AuthStateListener authStateListener;
     Toolbar logintoolbar;
+    CallbackManager callbackmanager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.fullyInitialize();
+        callbackmanager = CallbackManager.Factory.create();
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        if (token != null){
+            handelFacebookResponse(token);
+        }
         setContentView(R.layout.activity_login);
         logintoolbar = findViewById(R.id.login_activity_toolbar);
         setSupportActionBar(logintoolbar);
@@ -98,5 +123,66 @@ public class LoginActivity extends AppCompatActivity {
 
         });
 
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setPermissions(Collections.singletonList("email"));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackmanager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                handelFacebookResponse(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("DEBUG", "canceled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.i("DEBUG", exception.toString());
+            }
+        });
+
+        loginButton.callOnClick();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackmanager.onActivityResult(requestCode, resultCode, data);
+
+//        AccessToken token = AccessToken.getCurrentAccessToken();
+//        if (token != null){
+//            handelFacebookResponse(token);
+//        }
+    }
+
+    private void handelFacebookResponse(AccessToken token) {
+
+        Log.i("DEBUG", "token: " + token.getToken());
+
+        GraphRequest request = GraphRequest.newMeRequest(
+                token, (object, response) -> {
+
+                    try {
+                        String first_name = object.getString("first_name");
+                        String last_name = object.getString("last_name");
+                        String email = object.getString("email");
+
+                        Log.i("DEBUG", email);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
     }
 }
